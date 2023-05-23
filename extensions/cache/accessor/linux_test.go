1d078f5889a6397f76b361b83c6bf3d40b0f9a5f
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2023-05-22 17:06:20 -0700

    tests require cgo too

--- extensions/cache/accessor/linux_test.go
+++ extensions/cache/accessor/linux_test.go
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-//go:build linux
-// +build linux
+//go:build linux && cgo
+// +build linux,cgo
 
 package accessor
 
--- extensions/cache/accessor/storage_test.go
+++ extensions/cache/accessor/storage_test.go
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-//go:build linux || windows
-// +build linux windows
+//go:build (linux && cgo) || windows
+// +build linux,cgo windows
 
 package accessor
 
