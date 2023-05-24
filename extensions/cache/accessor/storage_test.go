36d76ddb9cfaa7ba0ea298b59e26548f8536c0d6
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2023-05-23 18:58:02 -0700

    macOS storage accessor

--- extensions/cache/accessor/storage_test.go
+++ extensions/cache/accessor/storage_test.go
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-//go:build (linux && cgo) || windows
-// +build linux,cgo windows
+//go:build (darwin && cgo) || (linux && cgo) || windows
+// +build darwin,cgo linux,cgo windows
 
 package accessor
 
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -53,4 +54,74 @@ func TestReadWrite(t *testing.T) {
 			require.Equal(t, test.want, actual)
 		})
 	}
+}
+
+func TestEmpty(t *testing.T) {
+	if !manualTests {
+		t.Skipf("set %s to run this test", msalextManualTest)
+	}
+	s := t.Name()
+	if runtime.GOOS != "darwin" {
+		s = filepath.Join(t.TempDir(), s)
+	}
+	a, err := New(s)
+	require.NoError(t, err)
+
+	actual, err := a.Read(ctx)
+	require.NoError(t, err)
+	require.Nil(t, actual)
+}
+
+func TestRace(t *testing.T) {
+	if !manualTests {
+		t.Skipf("set %s to run this test", msalextManualTest)
+	}
+	s := t.Name()
+	if runtime.GOOS != "darwin" {
+		s = filepath.Join(t.TempDir(), s)
+	}
+	a, err := New(s)
+	require.NoError(t, err)
+
+	expected := "expected"
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if !t.Failed() {
+				actual := []byte{}
+				err := a.Write(ctx, []byte(expected))
+				if err == nil {
+					actual, err = a.Read(ctx)
+				}
+				if err != nil {
+					t.Error(err)
+				} else if a := string(actual); a != expected {
+					t.Errorf("expected %q, got %q", expected, a)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestRoundTrip(t *testing.T) {
+	if !manualTests {
+		t.Skipf("set %s to run this test", msalextManualTest)
+	}
+	s := t.Name()
+	if runtime.GOOS != "darwin" {
+		s = filepath.Join(t.TempDir(), s)
+	}
+	a, err := New(s)
+	require.NoError(t, err)
+
+	expected := []byte("expected2")
+	err = a.Write(ctx, expected)
+	require.NoError(t, err)
+
+	actual, err := a.Read(ctx)
+	require.NoError(t, err)
+	require.Equal(t, expected, actual)
 }
