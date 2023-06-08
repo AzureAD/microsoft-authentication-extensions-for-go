5ae5202081030e378b498605a6a8697e9b0b481a
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2023-06-08 09:38:26 -0700

    Windows storage accessor (#15)

---
+++ extensions/cache/accessor/storage_test.go
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// TODO: add other platforms
+//go:build windows
+// +build windows
+
+package accessor
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const msalextManualTest = "MSALEXT_MANUAL_TEST"
+
+var (
+	ctx = context.Background()
+
+	// the Windows implementation doesn't require user interaction
+	manualTests = runtime.GOOS == "windows" || os.Getenv(msalextManualTest) != ""
+)
+
+func TestReadWrite(t *testing.T) {
+	if !manualTests {
+		t.Skipf("set %s to run this test", msalextManualTest)
+	}
+	for _, test := range []struct {
+		desc string
+		want []byte
+	}{
+		{desc: "Test when no stored data exists"},
+		{desc: "Test writing data then reading it", want: []byte("want")},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			p := filepath.Join(t.TempDir(), t.Name())
+			a, err := New(p)
+			require.NoError(t, err)
+
+			if test.want != nil {
+				cp := make([]byte, len(test.want))
+				copy(cp, test.want)
+				err = a.Write(ctx, cp)
+				require.NoError(t, err)
+			}
+
+			actual, err := a.Read(ctx)
+			require.NoError(t, err)
+			require.Equal(t, test.want, actual)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.0.0
 	github.com/stretchr/testify v1.8.2
+	golang.org/x/sys v0.8.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
 )
 
@@ -17,6 +18,5 @@ require (
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
