7f9622564771e57ae6f17c0032772f7785af25f5
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2023-06-08 09:15:34 -0700

    Cache and accessor/file packages (#14)

---
+++ extensions/cache/accessor/file/file.go
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+package file
+
+import (
+	"context"
+	"errors"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/AzureAD/microsoft-authentication-extensions-for-go/extensions/cache/accessor"
+)
+
+// Storage stores data in an unencrypted file.
+type Storage struct {
+	m *sync.RWMutex
+	p string
+}
+
+// New is the constructor for Storage. "p" is the path to the file in which to store data.
+func New(p string) (*Storage, error) {
+	return &Storage{m: &sync.RWMutex{}, p: p}, nil
+}
+
+// Read returns the file's content or, if the file doesn't exist, a nil slice and error.
+func (s *Storage) Read(context.Context) ([]byte, error) {
+	s.m.RLock()
+	defer s.m.RUnlock()
+	b, err := os.ReadFile(s.p)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, nil
+	}
+	return b, err
+}
+
+// Write stores data in the file, overwriting any content, and creates the file if necessary.
+func (s *Storage) Write(ctx context.Context, data []byte) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+	err := os.WriteFile(s.p, data, 0600)
+	if errors.Is(err, os.ErrNotExist) {
+		dir := filepath.Dir(s.p)
+		if err = os.MkdirAll(dir, 0700); err == nil {
+			err = os.WriteFile(s.p, data, 0600)
+		}
+	}
+	return err
+}
+
+var _ accessor.Accessor = (*Storage)(nil)
---
+++ extensions/perf_test.go
@@ -0,0 +1,99 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+package extensions
+
+import (
+	"fmt"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/AzureAD/microsoft-authentication-extensions-for-go/extensions/cache"
+	"github.com/AzureAD/microsoft-authentication-extensions-for-go/extensions/cache/accessor/file"
+	mcache "github.com/AzureAD/microsoft-authentication-library-for-go/apps/cache"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/public"
+	"github.com/stretchr/testify/require"
+)
+
+// this file benchmarks MSAL clients using Cache and file.Accessor
+
+func newCache(b *testing.B) *cache.Cache {
+	p := filepath.Join(b.TempDir(), b.Name())
+	a, err := file.New(p)
+	require.NoError(b, err)
+	c, err := cache.New(a, p+".timestamp")
+	require.NoError(b, err)
+	return c
+}
+
+func BenchmarkConfidentialClient(b *testing.B) {
+	for _, baseline := range []bool{false, true} {
+		name := "file accessor"
+		if baseline {
+			name = "no persistence"
+		}
+		b.Run(name, func(b *testing.B) {
+			var c mcache.ExportReplace
+			if !baseline {
+				c = newCache(b)
+			}
+			cred, err := confidential.NewCredFromSecret("*")
+			require.NoError(b, err)
+			client, err := confidential.New(
+				"https://login.microsoftonline.com/tenant", "ID", cred, confidential.WithCache(c), confidential.WithHTTPClient(&mockSTS{}),
+			)
+			require.NoError(b, err)
+
+			gr := 10
+			wg := sync.WaitGroup{}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for i := 0; i < gr; i++ {
+					wg.Add(1)
+					go func(n int) {
+						defer wg.Done()
+						s := fmt.Sprint(n)
+						_, _ = client.AcquireTokenByCredential(ctx, []string{s})
+						_, _ = client.AcquireTokenSilent(ctx, []string{s})
+					}(i)
+				}
+				wg.Wait()
+			}
+		})
+	}
+}
+
+func BenchmarkPublicClient(b *testing.B) {
+	for _, baseline := range []bool{false, true} {
+		name := "file accessor"
+		if baseline {
+			name = "no persistence"
+		}
+		b.Run(name, func(b *testing.B) {
+			var c mcache.ExportReplace
+			if !baseline {
+				c = newCache(b)
+			}
+			client, err := public.New("clientID", public.WithCache(c), public.WithHTTPClient(&mockSTS{}))
+			require.NoError(b, err)
+
+			gr := 10
+			wg := sync.WaitGroup{}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for i := 0; i < gr; i++ {
+					wg.Add(1)
+					go func(n int) {
+						defer wg.Done()
+						s := fmt.Sprint(n)
+						ar, _ := client.AcquireTokenByUsernamePassword(ctx, []string{s}, s, "password")
+						_, _ = client.AcquireTokenSilent(ctx, []string{s}, public.WithSilentAccount(ar.Account))
+					}(i)
+				}
+				wg.Wait()
+			}
+		})
+	}
+}
